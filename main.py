266107240ad3e2989d5b16b0c303694362771b88
Gobi <IGS@IGS-0075.igsc.in>
2021-02-12 17:51:47 +0530

    web socket added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import Depends, FastAPI, Request
+from fastapi import Depends, FastAPI, Request, WebSocket, Query
 from fastapi_jwt_auth.exceptions import AuthJWTException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
+
+from fastapi_jwt_auth import AuthJWT
 
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
@@ -24,7 +26,8 @@ app.include_router(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Bigger Applications!"}
+    # return {"message": "Hello Bigger Applications!"}
+    return HTMLResponse(html)
 
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
@@ -32,3 +35,52 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
         status_code=exc.status_code,
         content={"detail": exc.message}
     )    
+
+# Web Socket Protecting
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Authorize</title>
+    </head>
+    <body>
+        <h1>WebSocket Authorize</h1>
+        <p>Token:</p>
+        <textarea id="token" rows="4" cols="50"></textarea><br><br>
+        <button onclick="websocketfun()">Send</button>
+        <ul id='messages'>
+        </ul>
+        <script>
+            const websocketfun = () => {
+                let token = document.getElementById("token").value
+                let ws = new WebSocket(`ws://127.0.0.1:8000/ws?token=${token}`)
+                ws.onmessage = (event) => {
+                    let messages = document.getElementById('messages')
+                    let message = document.createElement('li')
+                    let content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                }
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.websocket('/ws')
+async def websocket(websocket: WebSocket, token: str = Query(...), Authorize: AuthJWT = Depends()):
+    await websocket.accept()
+    try:
+        Authorize.jwt_required("websocket",token=token)
+        # Authorize.jwt_optional("websocket",token=token)
+        # Authorize.jwt_refresh_token_required("websocket",token=token)
+        # Authorize.fresh_jwt_required("websocket",token=token)
+        await websocket.send_text("Successfully Login!")
+        decoded_token = Authorize.get_raw_jwt(token)
+        await websocket.send_text(f"Here your decoded token: {decoded_token}")
+    except AuthJWTException as err:
+        await websocket.send_text(err.message)
+        await websocket.close()
